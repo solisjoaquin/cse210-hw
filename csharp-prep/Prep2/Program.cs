94393493b3251339be38f5d4e0de410dba0e2ef8
Joaquin Solis <65245306+solisjoaquin@users.noreply.github.com>
2023-06-10 15:12:53 +0000

    add prep 1 and 2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,29 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is Grade pertentage? ");
+        int percent = int.Parse(Console.ReadLine());
+
+        if (percent > 90)
+        {
+            Console.WriteLine("Your grade is: A");
+        }
+        else if (percent >= 80)
+        {
+            Console.WriteLine("Your grade is: B");
+        }
+        else if (percent >= 70)
+        {
+            Console.WriteLine("Your grade is: C");
+        }
+        else if (percent >= 60)
+        {
+            Console.WriteLine("Your grade is: D");
+        }
+        else
+        {   
+            Console.WriteLine("Your grade is: F");
+        }
+
     }
 }
