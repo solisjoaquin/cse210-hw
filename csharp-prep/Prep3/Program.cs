57ec6cb69cfbcd457c744c8d7ec55ede3a79b6ce
Joaquin Solis <65245306+solisjoaquin@users.noreply.github.com>
2023-06-11 03:40:46 +0000

    Add preps

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,28 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomGenerator = new Random();
+        int number = randomGenerator.Next(1, 11);
+
+        Console.Write("What is your guess? ");
+        int response = int.Parse(Console.ReadLine());
+
+        do
+        {
+            if (response < number)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (response > number)
+            {
+                Console.WriteLine("Lower");
+            }
+
+            Console.Write("What is your guess? ");
+            response = int.Parse(Console.ReadLine());
+
+        } while (response != number);
+
+        Console.WriteLine("You guessed it!");
     }
 }
